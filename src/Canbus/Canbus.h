b4b65b6219249493f1455cee2c0c759347885463
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-10-11 19:02:18 -0300

    chore: add canbus packed

--- src/Canbus/Canbus.h
+++ src/Canbus/Canbus.h
@@ -13,6 +13,23 @@ class Canbus
         void tick();
 
     private:
+        struct PACKED {
+            uint32_t counter;
+            uint16_t throttle_req;
+            uint16_t throttle;
+            uint16_t rpm;
+            uint16_t voltage;
+            int16_t current;
+            int16_t phase_current;
+            uint8_t mos_temperature;
+            uint8_t cap_temperature;
+            uint16_t status;
+            uint16_t crc;
+        } pkt;
+
+        bool parsePacket(void);
+
+
 };
 
 #endif
