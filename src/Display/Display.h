dda04d1a6f8db1fb8a6ef329bc4da4c07360e474
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-11-02 19:56:02 -0300

    chore: clean display

--- src/Display/Display.h
+++ src/Display/Display.h
@@ -9,14 +9,6 @@ class Display : public U8G2_SH1106_128X64_NONAME_1_HW_I2C
 {
 public:
     Display() : U8G2_SH1106_128X64_NONAME_1_HW_I2C(U8G2_R0, U8X8_PIN_NONE) {};
-
-    void printCenter(char *text, uint8_t textX, uint8_t textY);
-    void printCenter(int value, uint8_t textX, uint8_t textY);
-    void printCenter(float value, uint8_t textX, uint8_t textY);
-
-    void printRight(char *text, uint8_t textX, uint8_t textY);
-    void printRight(int value, uint8_t textX, uint8_t textY);
-    void printRight(float value, uint8_t textX, uint8_t textY);
 };
 
 #endif
