f33b413f19ac0548e3dd09ccbdc7f56dd077d5ce
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-11-03 00:36:10 -0300

    chore: remove delay from temperature class

--- src/Temperature/Temperature.cpp
+++ src/Temperature/Temperature.cpp
@@ -7,7 +7,6 @@ double Temperature::readTemperature() {
   int sum = 0;
   for (int i = 0; i < nSamples; i++) {
     sum += analogRead(MOTOR_TEMPERATURE_PIN);
-    delay (10);
   }
   
   double v = (vcc*sum)/(nSamples*1024.0);
