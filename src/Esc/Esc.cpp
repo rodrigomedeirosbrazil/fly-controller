475cf1618fdc55dec1319f5b3663d577802e25f3
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-01-20 21:44:06 -0300

    chore: add Esc class

---
+++ src/Esc/Esc.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "Esc.h"
+
+#include "../config.h"
+
+Esc::Esc() {
+}
+
+void Esc::handle()
+{
+}
---
+++ src/Esc/Esc.h
@@ -0,0 +1,14 @@
+#ifndef Esc_h
+#define Esc_h
+
+#include "../config.h"
+
+class Esc {
+    public:
+        Esc();
+        void handle();
+
+
+    private:
+
+};
