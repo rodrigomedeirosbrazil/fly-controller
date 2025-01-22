a7cdd9b8b3b5817f45390687874f7e2acc2a8eb9
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-01-21 21:28:08 -0300

    feat: add setPowerAvailable method and lastPowerAvailableChange tracking to Esc class

--- src/Esc/Esc.h
+++ src/Esc/Esc.h
@@ -11,14 +11,16 @@ class Esc {
         Esc(Canbus *canbus, Throttle *throttle);
         void handle();
         unsigned int getPowerAvailable() { return powerAvailable; }
-
+        void setPowerAvailable(unsigned int power);
 
     private:
+        const static unsigned long minTimeToChangePowerAvailable = 1000;
+
         Servo esc;
         Canbus *canbus;
         Throttle *throttle;
 
         unsigned int powerAvailable;
-
+        unsigned long lastPowerAvailableChange;
 };
 #endif
