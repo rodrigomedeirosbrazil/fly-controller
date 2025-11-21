3dc6c0e00aab02ab82e0268690a62e050b867381
Rodrigo Medeiros <rodrigo@medeirostec.com.br>
2025-11-20 22:55:32 -0300

    refactor: Remove comment from WebServer header inclusion in main.h for cleaner code

--- src/main.h
+++ src/main.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include <AceButton.h>
 
-#include "WebServer/ControllerWebServer.h" // Include our WebServer header
+#include "WebServer/ControllerWebServer.h"
 
 using namespace ace_button;
 
@@ -14,6 +14,6 @@ void handleButtonEvent(AceButton* aceButton, uint8_t eventType, uint8_t buttonSt
 bool isMotorRunning();
 void handleArmedBeep();
 
-extern ControllerWebServer webServer; // Declare extern instance of WebServer
+extern ControllerWebServer webServer;
 
 #endif
